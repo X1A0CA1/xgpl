97c9a09d6e756f41f7aed11a0eacd6c3544c9dfc
xiaocai <52635310+X1A0CA1@users.noreply.github.com>
2024-04-14 15:43:13 +0800

    fix: 修复下载tun2socks时的问题

--- install.sh
+++ install.sh
@@ -144,7 +144,8 @@ function install_client {
     elif [[ "$_arch" == "arm64" ]]; then
         tun2socks_url="https://github.com/xjasonlyu/tun2socks/releases/download/v2.5.2/tun2socks-linux-arm64.zip"
     fi
-    wget -qO- $tun2socks_url | unzip -d /usr/local/bin/ - >> /dev/null 2>&1
+    wget -qO- $tun2socks_url -O /tmp/tun2socks.zip >> /dev/null 2>&1
+    unzip -q /tmp/tun2socks.zip -d /usr/local/bin/ >> /dev/null 2>&1
     chmod +x /usr/local/bin/tun2socks
     echo "下载 Tun2socks 完成"
     echo "下载 xgpl..."
